f89f619393dfefe53a0c6ebeb97336b74d06d49a
Rut <rutwintzell@outlook.com>
2026-03-03 15:03:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,39 @@ app.get('/recipes/:id', (req, res) => {
     res.json(recipe);
 });
 
+// POST - Add a new recipe
+app.post('/recipes', (req, res) => {
+    const newRecipe = {
+        id: recipes.length + 1,
+        name: req.body.name,
+        cuisine: req.body.cuisine,
+        prepTime: req.body.prepTime
+    };
+    recipes.push(newRecipe);
+    res.status(201).send({ message: "Recipe added!", recipe: newRecipe });
+});
+
+// PUT - Update an existing recipe
+app.put('/recipes/:id', (req, res) => {
+    const recipe = recipes.find(r => r.id === parseInt(req.params.id));
+    if (!recipe) return res.status(404).send({ message: "Recipe not found" });
+
+    recipe.name = req.body.name || recipe.name;
+    recipe.cuisine = req.body.cuisine || recipe.cuisine;
+    recipe.prepTime = req.body.prepTime || recipe.prepTime;
+
+    res.send({ message: "Recipe updated!", recipe });
+});
+
+// DELETE - Remove a recipe
+app.delete('/recipes/:id', (req, res) => {
+    const index = recipes.findIndex(r => r.id === parseInt(req.params.id));
+    if (index === -1) return res.status(404).send({ message: "Recipe not found" });
+
+    const deletedRecipe = recipes.splice(index, 1);
+    res.send({ message: "Recipe deleted successfully", deletedRecipe });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
